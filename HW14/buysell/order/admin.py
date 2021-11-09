67a33a7a461760f32a995f9e8f6057803ece5542
moeinmir <moein.mirani@gmail.com>
2021-11-09 22:52:32 +0330

    14000818

--- HW14/buysell/order/admin.py
+++ HW14/buysell/order/admin.py
@@ -5,6 +5,10 @@ from .models import Category
 from .models import Order
 from .models import Buyers
 from .models import ListOfInt
+from .models import BuyyerFactor
+from .models import SellerFactor
+from .models import BuyyerEmail
+from .models import SellerEmail
 
 # Register your models here.
 
@@ -14,3 +18,8 @@ admin.site.register(Category)
 admin.site.register(Order)
 admin.site.register(Buyers)
 admin.site.register(ListOfInt)
+admin.site.register(BuyyerFactor)
+admin.site.register(SellerFactor)
+admin.site.register(BuyyerEmail)
+admin.site.register(SellerEmail)
+
--- HW14/buysell/order/models.py
+++ HW14/buysell/order/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.fields import TextField
 
 
 # Create your models here.
@@ -38,6 +40,34 @@ class ListOfCom(models.Model):
 
 status_choices = (('PAY', "payed"),('NOT', "notpayed"))
 
+class BuyyerFactor(models.Model):
+    factor_number=models.PositiveIntegerField()
+    cost=models.PositiveIntegerField()
+    factor_pub_date=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return str(str(self.factor_number)+" "+str(self.cost))
+
+
+
+class SellerFactor(models.Model):
+    factor_number=models.PositiveIntegerField()
+    cost=models.PositiveIntegerField()
+    factor_pub_date=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return str(str(self.factor_number)+" "+str(self.cost))
+
+
+
+class BuyyerEmail(models.Model):
+    email_text=models.TextField()
+    Buyers = models.ForeignKey(Buyers, on_delete=models.PROTECT)
+    BuyyerFactor = models.ForeignKey(BuyyerFactor, on_delete=models.PROTECT)
+
+
+class SellerEmail(models.Model):
+    email_text=models.TextField()
+    Sellers = models.ForeignKey(Sellers, on_delete=models.PROTECT)
+    SellerFactor = models.ForeignKey(SellerFactor, on_delete=models.PROTECT)
 
 
 
@@ -46,6 +76,8 @@ status_choices = (('PAY', "payed"),('NOT', "notpayed"))
 class Order(models.Model):
     buyer = models.ForeignKey(Buyers, on_delete=models.CASCADE)
     comodity = models.ForeignKey(ListOfCom, on_delete=models.PROTECT)
+    BuyyerFactor=models.ForeignKey(BuyyerFactor,null=True,blank=True,on_delete=models.PROTECT)
+    SellerFactor=models.ForeignKey(SellerFactor,null=True,blank=True,on_delete=models.PROTECT)
     number = models.PositiveIntegerField()
     cost = models.PositiveIntegerField()
     order_date = models.DateTimeField(auto_now_add=True)
@@ -62,5 +94,7 @@ class ListOfInt(models.Model):
     buyer = models.ForeignKey(Buyers, on_delete=models.CASCADE)
     comodity = models.ForeignKey(ListOfCom, on_delete=models.CASCADE)
     puttointrest_date = models.DateTimeField(auto_now_add=True)
+
+
 
 
