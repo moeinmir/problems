e280f7626cddfb2e31e56a90f01bba87e0d6d3ee
moeinmir <moein.mirani@gmail.com>
2022-03-08 06:33:08 -0800

    week 2 start

--- 14001217_solving_al_week2_queues/Deque.java
+++ 14001217_solving_al_week2_queues/Deque.java
@@ -1,76 +1,60 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
-
-import java.util.Iterator;
+// Java program to show working of user defined
+// Generic classes
+// We use < > to specify Parameter type
+// class Deque<T> {
+//     // An object of type T is declared
+//     T obj;
+//
+//     Deque(T obj) {
+//         this.obj = obj;
+//     } // constructor
+//
+//     public T getObject() {
+//         return this.obj;
+//     }
+//
+//     public static void main(String[] args) {
+//         // instance of Integer type
+//         Deque<Integer> iObj = new Deque<Integer>(15);
+//         System.out.println(iObj.getObject());
+//
+//         // instance of String type
+//         Deque<String> sObj
+//                 = new Deque<String>("GeeksForGeeks");
+//         System.out.println(sObj.getObject());
+//     }
+// }
 
 public class Deque<Item> implements Iterable<Item> {
-    private Node first = null;
-    private Node last = null;
-
-    private class Node {
-        String item;
-        Node next;
-    }
-
-
-    // construct an empty deque
-    public Deque()
-
-    // is the deque empty?
-    public boolean isEmpty() {
-        return first == null
-    }
-
-    // return the number of items on the deque
-    public int size()
-
-    // add the item to the front
-    public void addFirst(Item item) {
-        Node oldfirst = first;
-        first = new Node();
-        first.item = item;
-        first.next = oldfirst;
-    }
-
-    // add the item to the back
-    public void addLast(Item item) {
-        Node oldfirst = first;
-        first = new Node();
-        first.item = item;
-        first.next = oldfirst;
-
-    }
-
-    // remove and return the item from the front
-    public Item removeFirst()
-
-    // remove and return the item from the back
-    public Item removeLast() {
-        String item = first.item;
-        first = first.next;
-        return item;
-    }
-
-    // return an iterator over items in order from front to back
-    public Iterator<Item> iterator() {
-        private Node current = first;
-        public boolean hasNext () {
-            return current != null;
-        }
-        public Item next ()
-        {
-            Item item = current.item;
-            current = current.next;
-            return item;
-        }
-    }
-
-    // unit testing (required)
-    public static void main(String[] args) {
-
-    }
-
+    //
+    //     // construct an empty deque
+    //     public Deque()
+    //
+    //     // is the deque empty?
+    //     public boolean isEmpty()
+    //
+    //     // return the number of items on the deque
+    //     public int size()
+    //
+    //     // add the item to the front
+    //     public void addFirst(Item item)
+    //
+    //     // add the item to the back
+    //     public void addLast(Item item)
+    //
+    //     // remove and return the item from the front
+    //     public Item removeFirst()
+    //
+    //     // remove and return the item from the back
+    //     public Item removeLast()
+    //
+    //     // return an iterator over items in order from front to back
+    //     public Iterator<Item> iterator()
+    //
+    //     // unit testing (required)
+    //     public static void main(String[] args)
+    //
 }
+
+
+
