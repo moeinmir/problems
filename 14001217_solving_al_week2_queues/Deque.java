31576cebf02dff58cfc6a2aaa4fe51ced396c2b6
moeinmir <moein.mirani@gmail.com>
2022-03-09 00:37:12 -0800

    deque of week two solved, i have got familiar with generics in java

--- 14001217_solving_al_week2_queues/Deque.java
+++ 14001217_solving_al_week2_queues/Deque.java
@@ -1,16 +1,18 @@
-public class Deque<Item> {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+public class Deque<Item> implements Iterable<Item> {
     public class Node {
         public Item item;
         public Node next;
+        public Node prev;
     }
 
     public Node first;
     public Node last;
     public Node current;
     public int size;
-    
-    //     // construct an empty deque
+
     public Deque() {
         first = null;
         last = null;
@@ -18,41 +20,53 @@ public class Deque<Item> {
         current = null;
     }
 
-    //
-    //     // is the deque empty?
     public boolean isEmpty() {
         return (size == 0);
     }
 
-    //
-    //     // return the number of items on the deque
     public int size() {
         return size;
     }
 
-    //
-    //     // add the item to the front
     public void addFirst(Item item) {
-        Node oldlast = last;
-        last = new Node();
-        last.item = item;
-        last.next = null;
-        if (isEmpty()) first = last;
-        else oldlast.next = last;
+        if (isEmpty()) {
+            first = new Node();
+            first.item = item;
+            first.next = null;
+            first.prev = null;
+            last = first;
+        }
+        else {
+            // current = new Node();
+            Node oldfirst = first;
+            first = new Node();
+            first.item = item;
+            first.next = null;
+            first.next = oldfirst;
+            oldfirst.prev = first;
+            first.prev = null;
+        }
         size = size + 1;
     }
 
-    //
-    //     // add the item to the back
     public void addLast(Item item) {
-        Node oldfirst = first;
-        first = new Node();
-        first.item = item;
-        first.next = oldfirst;
+        if (isEmpty()) {
+            first = new Node();
+            first.item = item;
+            first.next = null;
+            first.prev = null;
+            last = first;
+        }
+        else {
+            Node oldlast = last;
+            last = new Node();
+            last.item = item;
+            oldlast.next = last;
+            last.prev = oldlast;
+        }
+        size = size + 1;
     }
 
-    //
-    //     // remove and return the item from the front
     public Item removeFirst() {
         Item item = first.item;
         first = first.next;
@@ -60,34 +74,59 @@ public class Deque<Item> {
         return item;
     }
 
-    //
-    //     // remove and return the item from the back
     public Item removeLast() {
-        Item item = first.item;
-        first = first.next;
+        Item item = last.item;
+        last = last.prev;
+        last.next = null;
+        System.out.println("%%%%%%%%%%");
+        System.out.println(item);
+        System.out.println("************");
         return item;
     }
-    //
-    //     // return an iterator over items in order from front to back
-    // public Iterator<Item> iterator() {
-    //     System.out.println("hello");
-    // }
 
-    //
-    //     // unit testing (required)
+    public Iterator<Item> iterator() {
+        System.out.println("hello");
+        return new NewIterator();
+    }
+
+    private class NewIterator implements Iterator<Item> {
+        private Node current1 = first;
+
+        public boolean hasNext() {
+            return current1 != null;
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Item item = current1.item;
+            current1 = current1.next;
+            return item;
+        }
+
+    }
+
     public static void main(String[] args) {
         System.out.println("hello");
         Deque<Integer> my_object = new Deque<Integer>();
-        for (int i = 0; i < 10; i++) {
-            my_object.addFirst(i);
-        }
-        my_object.current = my_object.first;
-        for (int i = 0; i < 5; i++) {
-            my_object.current = my_object.current.next;
-            System.out.println(my_object.current.item);
+        my_object.addFirst(100);
+        my_object.addFirst(101);
+        my_object.addFirst(102);
+        my_object.addFirst(103);
+        my_object.addFirst(104);
+        my_object.addFirst(105);
+        my_object.addFirst(107);
+        my_object.addLast(110);
+        my_object.addLast(108);
+        my_object.removeFirst();
+        my_object.removeLast();
+
+        Iterator<Integer> my_iter = my_object.iterator();
+        while (my_iter.hasNext()) {
+            System.out.println(my_iter.next());
         }
     }
-    //
 }
 
 
